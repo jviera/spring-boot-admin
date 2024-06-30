c479ef138c1913d2f1aaa19efbb0ff1a5ddf0eaa
Jose Viera <jb.viera@gmail.com>
2024-06-30 17:50:59 -0600

    Initial commit

---
+++ src/main/java/dev/jviera/metrics_server/MetricsServerApplication.java
@@ -0,0 +1,15 @@
+package dev.jviera.metrics_server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class MetricsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsServerApplication.class, args);
+	}
+
+}
